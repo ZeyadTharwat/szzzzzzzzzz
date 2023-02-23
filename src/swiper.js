25208fd85a2156d247525671d19e826cd161cda9
ZeyadTharwat <zeyadtharwatcf@gmail.com>
2023-02-23 21:00:42 +0200

    Yes

--- src/swiper.js
+++ src/swiper.js
@@ -6,13 +6,18 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
   var swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 3,
+    slidesPerView: 2,
     spaceBetween: 0,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
+      375: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+
       490: {
         slidesPerView: 4,
         spaceBetween: 10,
